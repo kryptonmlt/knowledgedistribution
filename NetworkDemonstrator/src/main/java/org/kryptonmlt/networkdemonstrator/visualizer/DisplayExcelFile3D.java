b991878cbd7a100cb41d73b3d2209eea7af5df5d
Kurt Portelli <kurtportelli@gmail.com>
2016-06-15 21:31:27 +0100

    testing online sgd

--- NetworkDemonstrator/src/main/java/org/kryptonmlt/networkdemonstrator/visualizer/DisplayExcelFile3D.java
+++ NetworkDemonstrator/src/main/java/org/kryptonmlt/networkdemonstrator/visualizer/DisplayExcelFile3D.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Random;
 
@@ -13,43 +15,58 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jzy3d.colors.Color;
 import org.jzy3d.maths.Coord3d;
+import org.kryptonmlt.networkdemonstrator.learning.OnlineStochasticGradientDescent;
 
 public class DisplayExcelFile3D {
 
     public static void main(String args[]) throws IOException, Exception {
 
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
-        int sheets = 5;
+        int sheets = 1;
         ArrayList<Coord3d> coords = new ArrayList<>();
         ArrayList<Color> c = new ArrayList<>();
-        
+        Color[] choice = {Color.BLUE, Color.RED, Color.GREEN, Color.MAGENTA, Color.CYAN, Color.MAGENTA, Color.GRAY, Color.YELLOW};
+        OnlineStochasticGradientDescent sgd = new OnlineStochasticGradientDescent(0.01);
         FileInputStream file = new FileInputStream(new File("NormalizedData.xlsx"));
         //FileInputStream file = new FileInputStream(new File("Data.xlsx"));
         XSSFWorkbook workbook = new XSSFWorkbook(file);
-        Random r = new Random();
         for (int i = 0; i < sheets; i++) {
             XSSFSheet sheet = workbook.getSheetAt(i);
             Iterator<Row> rowIterator = sheet.iterator();
-            float red = r.nextFloat();
-            float green = r.nextFloat();
-            float blue = r.nextFloat();
-            
+
             while (rowIterator.hasNext()) {
                 Row row = rowIterator.next();
                 try {
-                    coords.add(new Coord3d(Double.parseDouble(row.getCell(0).getStringCellValue()),
-                            Double.parseDouble(row.getCell(1).getStringCellValue()), Double.parseDouble(row.getCell(2).getStringCellValue())));
+                    double x = Double.parseDouble(row.getCell(0).getStringCellValue());
+                    double y = Double.parseDouble(row.getCell(1).getStringCellValue());
+                    double z = Double.parseDouble(row.getCell(2).getStringCellValue());
+                    coords.add(new Coord3d(x, y, z));
+                    sgd.onlineSGD(x, y, z);
                 } catch (Exception e) {
                     coords.add(new Coord3d(sdf.parse(row.getCell(0).getStringCellValue()).getTime(),
                             Double.parseDouble(row.getCell(1).getStringCellValue()), Double.parseDouble(row.getCell(2).getStringCellValue())));
                 }
-                c.add(new Color(red, green, blue));
+                c.add(choice[i % choice.length]);
             }
         }
         file.close();
         Coord3d[] points = new Coord3d[coords.size()];
+        points = coords.toArray(points);
+        Arrays.sort(points, coord3dComparator);
+        Coord3d[] lineStrip = new Coord3d[coords.size()];
+        for (int i = 0; i < lineStrip.length; i++) {
+            float z = (float) sgd.predict(points[i].x, points[i].y);
+            lineStrip[i] = new Coord3d(points[i].x, points[i].y, z);
+        }
         Color[] colors = new Color[c.size()];
-        ScatterPlot3D plot = new ScatterPlot3D(coords.toArray(points), c.toArray(colors));
+        String[] names = {"Time", "PM25_AQI", "PM10_AQI"};
+        ScatterPlot3D plot = new ScatterPlot3D(points, c.toArray(colors), names, lineStrip);
         plot.show();
     }
+
+    public static Comparator<Coord3d> coord3dComparator = (Coord3d c1, Coord3d c2) -> {
+        Float x1 = c1.x;
+        Float x2 = c2.x;
+        return x1.compareTo(x2);
+    };
 }
--- NetworkDemonstrator/src/main/java/org/kryptonmlt/networkdemonstrator/visualizer/ScatterPlot3D.java
+++ NetworkDemonstrator/src/main/java/org/kryptonmlt/networkdemonstrator/visualizer/ScatterPlot3D.java
@@ -1,11 +1,16 @@
 package org.kryptonmlt.networkdemonstrator.visualizer;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.jzy3d.analysis.AbstractAnalysis;
 import org.jzy3d.analysis.AnalysisLauncher;
 import org.jzy3d.chart.factories.AWTChartComponentFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.maths.Coord3d;
+import org.jzy3d.plot3d.primitives.CroppableLineStrip;
+import org.jzy3d.plot3d.primitives.Point;
 import org.jzy3d.plot3d.primitives.Scatter;
+import org.jzy3d.plot3d.primitives.axes.layout.IAxeLayout;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
 
 /**
@@ -15,18 +20,41 @@ import org.jzy3d.plot3d.rendering.canvas.Quality;
 public class ScatterPlot3D extends AbstractAnalysis {
 
     private Coord3d[] points = null;
+    private Coord3d[] linePoints = null;
     private Color[] colors = null;
+    private String[] xyz;
 
     @Override
     public void init() {
-        Scatter scatter = new Scatter(points, colors);
+        Scatter scatter;
+        if (colors == null) {
+            scatter = new Scatter(points);
+        } else {
+            scatter = new Scatter(points, colors);
+        }
         chart = AWTChartComponentFactory.chart(Quality.Advanced, "newt");
+        IAxeLayout axeLayout = chart.getAxeLayout();
+        axeLayout.setXAxeLabel(xyz[0]);
+        axeLayout.setYAxeLabel(xyz[1]);
+        axeLayout.setZAxeLabel(xyz[2]);
         chart.getScene().add(scatter);
+        if (linePoints != null) {
+            List<CroppableLineStrip> lineStrips = new ArrayList<>();
+            CroppableLineStrip lineStrip = new CroppableLineStrip();
+            lineStrip.setWireframeColor(Color.BLACK);
+            for (Coord3d c : linePoints) {
+                lineStrip.add(new Point(c));
+            }
+            lineStrips.add(lineStrip);
+            chart.getScene().getGraph().add(lineStrips);
+        }
     }
 
-    public ScatterPlot3D(Coord3d[] points, Color[] colors) {
+    public ScatterPlot3D(Coord3d[] points, Color[] colors, String[] xyz, Coord3d[] linePoints) {
         this.points = points;
         this.colors = colors;
+        this.linePoints = linePoints;
+        this.xyz = xyz;
     }
 
     void show() throws Exception {
